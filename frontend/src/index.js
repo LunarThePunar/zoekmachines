7a48435993451b05bf2192812ca9be9dc60d17de
Chris Bras <chris.s.bras@gmail.com>
2019-09-30 14:40:51 +0200

    data to fill extra search fields is now requested from the server

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from "react-dom"
 import Sidebar from "react-sidebar";
 import Select from 'react-select';
 
+const axios = require('axios');
+
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
@@ -17,16 +19,98 @@ class App extends Component {
     super(props);
     this.state = {
       sidebarOpen: false,
-      selectedOption: null
+      selectedOption: null,
+      people: [],
+      orgs: [],
+      places: [],
+      topics: [],
+      exchanges: [],
     };
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     this.content = this.content.bind(this);
+
   }
 
   handleChange = selectedOption => {
     this.setState({ selectedOption });
     console.log(`Option selected:`, selectedOption);
   };
+
+  async loadPeople() {
+    try {
+      const response = await axios.get("/api/people");
+      const values = response.data.map(x => ({value: x, label: x}));
+      this.setState({
+        people: values
+      });
+    } catch(error) {
+      console.log("ERROR: Failed to update people data.");
+      console.log(error);  
+    }
+  }
+
+  async loadOrgs() {
+    try {
+      const response = await axios.get("/api/orgs");
+      const values = response.data.map(x => ({value: x, label: x}));
+      this.setState({
+        orgs: values
+      });
+    } catch(error) {
+      console.log("ERROR: Failed to update organisations data.");
+      console.log(error);  
+    }
+  }
+
+  async loadTopics() {
+    try {
+      const response = await axios.get("/api/topics");
+      const values = response.data.map(x => ({value: x, label: x}));
+      this.setState({
+        topics: values
+      });
+    } catch(error) {
+      console.log("ERROR: Failed to update topics data.");
+      console.log(error);  
+    }
+  }
+
+  async loadPlaces() {
+    try {
+      const response = await axios.get("/api/places");
+      const values = response.data.map(x => ({value: x, label: x}));
+      this.setState({
+        places: values
+      });
+    } catch(error) {
+      console.log("ERROR: Failed to update places data.");
+      console.log(error);  
+    }
+  }
+
+
+  async loadExchanges() {
+    try {
+      const response = await axios.get("/api/exchanges");
+      const values = response.data.map(x => ({value: x, label: x}));
+      this.setState({
+        exchanges: values
+      });
+    } catch(error) {
+      console.log("ERROR: Failed to update exchange data.");
+      console.log(error);  
+    }
+  }
+
+
+
+  async componentDidMount() {
+    await this.loadPeople();
+    await this.loadOrgs();
+    await this.loadPlaces();
+    await this.loadTopics();
+    await this.loadExchanges();
+  }
 
   content () {
     return (
@@ -35,49 +119,41 @@ class App extends Component {
         <Select
           value={this.selectedOption}
           onChange={this.handleChange}
-          options={options}
+          options={this.state.places}
           isSearchable={true}
-          isMulti={false}
+          isMulti={true}
         />
         <h3 className="header">Topics</h3>
         <Select
           value={this.selectedOption}
           onChange={this.handleChange}
-          options={options}
+          options={this.state.topics}
           isSearchable={true}
-          isMulti={false}
+          isMulti={true}
         />
         <h3 className="header">People</h3>
         <Select
           value={this.selectedOption}
           onChange={this.handleChange}
-          options={options}
+          options={this.state.people}
           isSearchable={true}
-          isMulti={false}
+          isMulti={true}
         />
         <h3 className="header">Organisations</h3>
         <Select
           value={this.selectedOption}
           onChange={this.handleChange}
-          options={options}
+          options={this.state.orgs}
           isSearchable={true}
-          isMulti={false}
-        />
-        <h3 className="header">Companies</h3>
-        <Select
-          value={this.selectedOption}
-          onChange={this.handleChange}
-          options={options}
-          isSearchable={true}
-          isMulti={false}
+          isMulti={true}
         />
         <h3 className="header">Exchanges</h3>
         <Select
           value={this.selectedOption}
           onChange={this.handleChange}
-          options={options}
+          options={this.state.exchanges}
           isSearchable={true}
-          isMulti={false}
+          isMulti={true}
         />
       </div>
     );
@@ -95,14 +171,14 @@ class App extends Component {
         onSetOpen={this.onSetSidebarOpen}
         styles={{ sidebar: { background: "white", padding: "10px", minWidth: "350px"} }}
       >
-      <div class="index">
-        <div class="logo">
+      <div className="index">
+        <div className="logo">
         </div>
-        <div class="query">
-          <input class="searchbar" v-model="query"></input>
-          <div class="searchbutton">
-            <input type="button" value="Search" class="button"></input>
-            <input onClick={() => this.onSetSidebarOpen(true)} type="button" value="Advanced Search" class="button"></input>
+        <div className="query">
+          <input className="searchbar" v-model="query"></input>
+          <div className="searchbutton">
+            <input type="button" value="Search" className="button"></input>
+            <input onClick={() => this.onSetSidebarOpen(true)} type="button" value="Advanced Search" className="button"></input>
           </div>
         </div>
       </div>
